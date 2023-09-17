8aaa6c9801d86b7db6c4f834a59b52412f7656b2
Joey Palchak <joey.palchak@gmail.com>
2023-09-16 17:55:23 -0700

    add passing test, code for Triangle.Side1 SET

--- ShapeTracker.Tests/ModelTests/TriangleTests.cs
+++ ShapeTracker.Tests/ModelTests/TriangleTests.cs
@@ -29,5 +29,18 @@ namespace ShapeTracker.TestTools
       Assert.AreEqual(length1, result);
     }
 
+    // test 3
+    [TestMethod]
+    public void SetSide1_SetsValueOfSide1_Void()
+    {
+      // Arrange
+      Triangle newTriangle = new Triangle(3);
+      int newLength1 = 44;
+      // Act
+      newTriangle.Side1 = newLength1;
+      // Assert
+      Assert.AreEqual(newLength1, newTriangle.Side1);
+    }
+
   }
 }
